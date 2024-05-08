46016c36058176b1dc3426c48aff252f9583c076
Thismathis <121266527+Thismathis@users.noreply.github.com>
2024-05-08 01:00:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+      return {
+    name: 'Amanda',
+    age: 22,
+    skills: ['JavaScript', 'React.js', 'Node.js'],
+    details: {
+      country: 'Brazil',
+      city: 'São Paulo'
+    }
+  }
+}
